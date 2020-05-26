bbe682ab95cf0344d884ff82f9e8f8a12c29456e
oren sampson <oren.sampson@gmail.com>
2020-05-26 21:25:56 +0300

    compacting array: case countires with sub counties

--- src/store/oneCountry/actions.js
+++ src/store/oneCountry/actions.js
@@ -2,23 +2,38 @@ import Vue from "vue";
 import moment from "moment";
 
 const calcDiff = (arr, daysAmount) => {
-  const newArr = [];
+  if (arr.length > 2) {
+    const compactedArr = [];
+    compactedArr.push(arr[0]);
+    for (let i = 1; i < arr.length - 1; i++) {
+      if (compactedArr[compactedArr.length - 1].Date === arr[i].Date) {
+        compactedArr[compactedArr.length - 1].Confirmed += arr[i].Confirmed;
+        compactedArr[compactedArr.length - 1].Deaths += arr[i].Deaths;
+      } else {
+        compactedArr.push(arr[i]);
+      }
+    }
 
-  for (
-    let i = arr.length - 1, counter = 1;
-    counter <= daysAmount && i > 0;
-    i--
-  ) {
-    newArr.unshift({
-      date: arr[i].Date,
-      newCases: arr[i].Confirmed - arr[i - 1].Confirmed,
-      newDeaths: arr[i].Deaths - arr[i - 1].Deaths
-    });
+    console.log(compactedArr);
 
-    counter++;
+    const newArr = [];
+    for (
+      let i = compactedArr.length - 1, counter = 1;
+      counter <= daysAmount && i > 0;
+      i--
+    ) {
+      newArr.unshift({
+        date: compactedArr[i].Date,
+        newCases: compactedArr[i].Confirmed - compactedArr[i - 1].Confirmed,
+        newDeaths: compactedArr[i].Deaths - compactedArr[i - 1].Deaths
+      });
+
+      counter++;
+    }
+
+    return newArr;
   }
-
-  return newArr;
+  commit("setErrorMsg", "Array fetched is to short. please try again");
 };
 
 const formatDate = date => date + "T00:00:00Z";
