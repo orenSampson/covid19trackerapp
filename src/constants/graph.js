3c58ab6aa8947777070b675940543abeab883996
oren sampson <oren.sampson@gmail.com>
2020-06-01 17:39:33 +0300

    graph feature: moved startPage to Graph view

--- src/constants/graph.js
+++ src/constants/graph.js
@@ -1,3 +1,4 @@
 export default Object.freeze({
-  LAST_DAYS_OPTIONS: [14, 30]
+  LAST_DAYS_OPTIONS: [14, 30],
+  MODES: { lastDays: 0, dateRange: 1 }
 });
--- src/pages/Graph.vue
+++ src/pages/Graph.vue
@@ -1,23 +1,58 @@
 <template>
     <div>
-        <start-page></start-page>
+        <div class="text-bold">Choose LastDays Mode Or Choose Date Range Selection Mode:</div>
+        <button @click="selectedMode = modes.lastDays">LastDays Mode</button>
+        <button @click="selectedMode = modes.dateRange">Date Range Selection Mode</button>
+
+        <last-days-selection v-if="selectedMode === modes.lastDays"></last-days-selection>
+        <date-range-selection v-if="selectedMode === modes.dateRange"></date-range-selection>
+
+        <graph-by-country v-if="!errorMsg"></graph-by-country>
+        <div v-if="errorMsg" class="text-h5">
+            <span class="text-weight-bold">Error with fetching the data:</span>
+            {{ errorMsg }}
+        </div>
     </div>
 </template>
 
 <script>
-import StartPage from "components/graph/StartPage";
+import LastDaysSelection from "components/graph/LastDaysSelection";
+import DateRangeSelection from "components/graph/DateRangeSelection";
+import GraphByCountry from "components/graph/GraphByCountry";
+
+import consts from "src/constants/graph";
+import { mapActions, mapGetters } from "vuex";
+
 export default {
-    name: "PageGraph",
+    name: "Graph",
 
     components: {
-        StartPage
+        LastDaysSelection,
+        DateRangeSelection,
+        GraphByCountry
     },
 
     data() {
-        return {};
+        return {
+            modes: consts.MODES,
+            selectedMode: null
+        };
+    },
+
+    methods: {
+        ...mapActions("oneCountry", { setCountry: "setCountryAction" })
+    },
+
+    computed: {
+        ...mapGetters("oneCountry", ["errorMsg"])
+    },
+
+    mounted() {
+        this.setCountry(this.$route.params.country);
+    },
+
+    watch: {
+        mode() {}
     }
 };
 </script>
-
-<style scoped>
-</style>
