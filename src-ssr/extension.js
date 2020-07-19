07e9b5c05f2f9a6cdbc0a13cb0829ba953b232a8
oren sampson <oren.sampson@gmail.com>
2020-07-19 16:45:28 +0300

    package.json updated

--- src-ssr/extension.js
+++ src-ssr/extension.js
@@ -1,8 +1,5 @@
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
-
 const MONGODB_URI =
-  "mongodb+srv://oren:turivdcr10@cluster0-qcnha.mongodb.net/sample_restaurants?retryWrites=true&w=majority";
+  "mongodb+srv://new-user:turivdcr10@cluster0-qcnha.mongodb.net/covid19trackerapp?retryWrites=true&w=majority";
 /*
  * This file runs in a Node context (it's NOT transpiled by Babel), so use only
  * the ES6 features that are supported by your Node version. https://node.green/
@@ -17,26 +14,6 @@ const MONGODB_URI =
  */
 
 module.exports.extendApp = function({ app, ssr }) {
-  const store = new MongoDBStore({
-    uri: MONGODB_URI,
-    collection: "sessions"
-  });
-
-  app.use(
-    session({
-      secret: "my secret",
-      resave: false,
-      saveUninitialized: false,
-      store: store
-    })
-  );
-  app.get("/saveSession", (req, res) => {
-    req.session.check = 50;
-    res.send("SAVED!");
-  });
-  app.get("/getSession", (req, res) => {
-    res.send("" + req.session.check);
-  });
   /*
      Extend the parts of the express app that you
      want to use with development server too.
