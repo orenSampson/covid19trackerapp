4ae058b7aa4b252a3365307ae0e9eef04c9284d7
oren sampson <oren.sampson@gmail.com>
2020-08-09 21:40:02 +0300

    back: prepared admin DB with countries

--- src-ssr/extension.js
+++ src-ssr/extension.js
@@ -1,34 +1,11 @@
-const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-const { MONGODB_URI } = require("./constants/mongo");
 const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin");
-const AdminCountry = require("./models/adminCountry");
-
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-
-// const mongooseFunc = async () => {
-//   console.log("try connection");
-//   try {
-//     console.log("try connection1");
-//     await mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
-//     const adminCountry = new AdminCountry({
-//       country: "Oren",
-//       slug: "oren",
-//       isSelected: true
-//     });
-//     await adminCountry.save();
-//     console.log("connection OK");
-//   } catch (err) {
-//     console.log(err);
-//   }
-// };
-
-// mongooseFunc();
+const connectAndPrepareDB = require("./utils/connectAndPrepareDB");
 
 module.exports.extendApp = function({ app, ssr }) {
-  //   mongooseFunc();
+  connectAndPrepareDB();
   app.use(bodyParser.json());
   app.use("/auth", authRoutes);
   app.use("/admin", adminRoutes);
---
+++ src-ssr/utils/connectAndPrepareDB.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+const axios = require("axios");
+
+const { MONGODB_URI } = require("../constants/mongo");
+const AdminCountry = require("../models/adminCountry");
+
+module.exports = async () => {
+  let numDocs = 0;
+  let countriesArr;
+
+  try {
+    await mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+  } catch (err) {
+    return console.log(err);
+  }
+
+  try {
+    numDocs = await AdminCountry.find().countDocuments();
+    if (numDocs !== 0) {
+      return;
+    }
+  } catch (err) {
+    return console.log(err);
+  }
+
+  try {
+    countriesArr = await axios.get("https://api.covid19api.com/countries");
+    if (!countriesArr) {
+      return;
+    }
+  } catch (err) {
+    return console.log(err);
+  }
+
+  countriesArr = countriesArr.data;
+
+  for (const element of countriesArr) {
+    try {
+      const adminCountry = new AdminCountry({
+        country: element.Country,
+        slug: element.Slug,
+        isSelected: false
+      });
+      await adminCountry.save();
+    } catch (err) {
+      return console.log(err);
+    }
+  }
+};
