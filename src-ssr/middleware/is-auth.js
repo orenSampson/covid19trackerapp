6d130728639dd4ba86b70a27a31174644d49342f
oren sampson <oren.sampson@gmail.com>
2020-08-18 14:20:04 +0300

    back: isAuth middleware message update

--- src-ssr/middleware/is-auth.js
+++ src-ssr/middleware/is-auth.js
@@ -17,8 +17,8 @@ module.exports = (req, res, next) => {
     decodedToken = jwt.verify(token, ACCESS_TOKEN_SECRET);
   } catch (err) {
     return res
-      .status(serverError.status)
-      .json({ message: serverError.message });
+      .status(notAuthenticated.status)
+      .json({ message: notAuthenticated.message });
   }
   if (!decodedToken) {
     return res
