e74e9c1cec1b2a6fdfd244f4db5e454f915b7b5e
oren sampson <oren.sampson@gmail.com>
2020-11-13 15:12:18 +0200

    back: removed unnecessary check

--- src-ssr/utils/connectAndPrepareDB.js
+++ src-ssr/utils/connectAndPrepareDB.js
@@ -5,45 +5,53 @@ const { MONGODB_URI } = require("../constants/mongo");
 const AdminCountry = require("../models/adminCountry");
 
 module.exports = async () => {
-  let numDocs = 0;
-  let countriesArr;
+//   let numDocs = 0;
+//   let countriesArr;
 
   try {
-    await mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+    await mongoose.connect(MONGODB_URI, { 
+        useNewUrlParser: true, 
+        useCreateIndex: true, 
+        useUnifiedTopology: true,
+        useFindAndModify: false });
   } catch (err) {
     return console.log(err);
   }
 
-  try {
-    numDocs = await AdminCountry.find().countDocuments();
-    if (numDocs !== 0) {
-      return;
-    }
-  } catch (err) {
-    return console.log(err);
-  }
+//   try {
+//     numDocs = await AdminCountry.find().countDocuments();
+//     if (numDocs !== 0) {
+//       return;
+//     } else {
 
-  try {
-    countriesArr = await axios.get("https://api.covid19api.com/countries");
-    if (!countriesArr) {
-      return;
-    }
-  } catch (err) {
-    return console.log(err);
-  }
+//     }
+//   } catch (err) {
+//     return console.log(err);
+//   }
 
-  countriesArr = countriesArr.data;
+//   try {
+//     countriesArr = await axios.get("https://api.covid19api.com/countries");
+//     if (!countriesArr) {
+//       return;
+//     }
+//   } catch (err) {
+//     return console.log(err);
+//   }
 
-  for (const element of countriesArr) {
-    try {
-      const adminCountry = new AdminCountry({
-        country: element.Country,
-        slug: element.Slug,
-        isSelected: false
-      });
-      await adminCountry.save();
-    } catch (err) {
-      return console.log(err);
-    }
-  }
+//   countriesArr = countriesArr.data;
+
+//   for (const element of countriesArr) {
+//     try {
+//       const adminCountry = new AdminCountry({
+//         country: element.Country,
+//         slug: element.Slug,
+//         isSelected: false
+//       });
+//       await adminCountry.save();
+//     } catch (err) {
+//       return console.log(err);
+//     }
+//   }
+
+  // https://www.geeksforgeeks.org/mongoose-insertmany-function/
 };
