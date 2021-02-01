6dc098277dbc73eca0eff10ee57ed6a904f0f4d9
oren sampson <oren.sampson@gmail.com>
2021-02-01 20:21:11 +0200

    refactoring

--- src-ssr/controllers/admin.js
+++ src-ssr/controllers/admin.js
@@ -1,6 +1,3 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-
 const AdminCountry = require("../models/adminCountry");
 const User = require("../models/user");
 const {
